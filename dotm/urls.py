ffd46f704305a9ea0295614aeb427b5ebbaf0c43
Paolo Melchiorre <paolo@melchiorre.org>
2026-09-12 11:31:56 +0200

    Update to Django 6.1

--- dotm/urls.py
+++ dotm/urls.py
@@ -2,7 +2,7 @@
 URL configuration for dotm project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/6.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,15 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-
 from django.views.generic import TemplateView
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -41,4 +36,4 @@ urlpatterns = [
         TemplateView.as_view(template_name="dotm/funding.html"),
         name="funding",
     ),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
